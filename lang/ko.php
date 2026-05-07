cac85e388e7e867de844731d3acf27147320509f
Joe Huss <detain@interserver.net>
2026-05-07 05:56:41 -0400

    Add Korean locale for all libs (#257)

---
+++ lang/ko.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Korean translations for candy-lister.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'list.no_items'    => '항목 없음: 목록이 비어 있습니다',
+    'list.zero_viewport' => '뷰포트 너비 또는 높이가 0이어서 표시할 수 없음',
+];
