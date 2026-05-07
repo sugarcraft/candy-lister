12559258fe3af86e29a593bbf58eff9adb44ad1a
Joe Huss <detain@interserver.net>
2026-05-07 05:50:43 -0400

    Add Japanese locale for all libs (#254)

---
+++ lang/ja.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Japanese translations for candy-lister.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'list.no_items'    => '項目なし：リストは空です',
+    'list.zero_viewport' => 'ビューポートの幅または高さがゼロのため表示できません',
+];
